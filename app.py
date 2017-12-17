47cd995dcafe684c01b70ffb7916a4681f8c5006
Ashutosh <ashu.shaw09@mgail.com>
2017-12-18 00:47:19 +0530

    The Blogging App

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ashu565959'
+app.config['MYSQL_PASSWORD'] = '********'
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
